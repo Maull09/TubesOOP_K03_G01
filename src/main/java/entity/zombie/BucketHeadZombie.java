bb708908e787e367bfd0f3b8f8c45aa2b8d70e74
MreffyH <118194238+MreffyH@users.noreply.github.com>
2024-05-03 20:11:23 +0700

    feat: hasBucket, getHasBucket, setHasBucket, attack

--- src/main/java/entity/zombie/BucketHeadZombie.java
+++ src/main/java/entity/zombie/BucketHeadZombie.java
@@ -3,12 +3,22 @@ package src.main.java.entity.zombie;
 import src.main.java.entity.plant.Plant;
 
 public class BucketHeadZombie extends Zombie{
+    private boolean hasBucket;
+
     public BucketHeadZombie(String name, int health, int attackDamage, int attackSpeed, boolean is_aquatic) {
         super("Bucket Head Zombie", 300, 100, 1, false);
     }
     
+    public boolean getHasBucket() {
+        return hasBucket;
+    }
+
+    public void setHasBucket(boolean hasBucket){
+        this.hasBucket = hasBucket;
+    }
+
     @Override
     public void attack(Plant plant) {
-        plant.setHealth(plant.getHealth() - this.getAttackDamage());
+        super.attack(plant);
     }
 }
